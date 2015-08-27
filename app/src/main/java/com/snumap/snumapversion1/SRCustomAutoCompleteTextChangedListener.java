cb3bf136c63eaadceff3866c6e55cb9ff4eb36e3
RYU KI HYUN <rukeon@snu.ac.kr>
2015-08-27 18:27:16 +0900

    길찾기 검색바 하나 등록

--- app/src/main/java/com/snumap/snumapversion1/SRCustomAutoCompleteTextChangedListener.java
+++ app/src/main/java/com/snumap/snumapversion1/SRCustomAutoCompleteTextChangedListener.java
@@ -9,11 +9,11 @@ import android.widget.ArrayAdapter;
 /**
  * Created by rukeon01 on 2015-07-15.
  */
-public class CustomAutoCompleteTextChangedListener implements TextWatcher {
+public class SRCustomAutoCompleteTextChangedListener implements TextWatcher {
     public static final String TAG = "CustomAutoCompleteTextChangedListener.java";
     Context context;
 
-    public CustomAutoCompleteTextChangedListener(Context context){
+    public SRCustomAutoCompleteTextChangedListener(Context context){
         this.context = context;
     }
 
@@ -40,30 +40,30 @@ public class CustomAutoCompleteTextChangedListener implements TextWatcher {
         // if you want to see in the logcat what the user types
 //        Log.e(TAG, "User input: " + userInput);
 
-        MainActivity mainActivity = ((MainActivity) context);
+        SearchRouteActivity searchActivity = ((SearchRouteActivity) context);
         // setOnItemClickListener와의 겹치는 것 제한하기 위한 코드
-        if (mainActivity.myAutoComplete.isPerformingCompletion()) {
+        if (searchActivity.myAutoComplete.isPerformingCompletion()) {
             // An item has been selected from the list. Ignore.
             return;
         }
 
         // query the database based on the user input
-        mainActivity.item = mainActivity.db.read(userInput.toString());
+        searchActivity.item = searchActivity.db.read(userInput.toString());
 
-        if (mainActivity.item.size() == 0 ) {
+        if (searchActivity.item.size() == 0 ) {
             Log.e("여기로 들어오나?", "real?");
-            for (int i = 0; i < mainActivity.searchList.size(); i++) {
-                String searchData = mainActivity.searchList.get(i).toString();
+            for (int i = 0; i < searchActivity.searchList.size(); i++) {
+                String searchData = searchActivity.searchList.get(i).toString();
                 String keyWord = userInput.toString();
                 boolean isData = SoundSearch.matchString(searchData, keyWord); //검색할 대상 , 검색 키워드로  검색키워드가 검색대상에 있으면  true를 리턴해준다
                 if (isData) {
-                    mainActivity.item.add(searchData);//검색대상에 있으면 새로운 리스트를 만들어서 이름을 애드해준다
+                    searchActivity.item.add(searchData);//검색대상에 있으면 새로운 리스트를 만들어서 이름을 애드해준다
                 }
             }
         }
         // update the adapater
-        mainActivity.myAdapter.notifyDataSetChanged();
-        mainActivity.myAdapter = new ArrayAdapter<String>(mainActivity, R.layout.autocomplete_list_row, mainActivity.item);
-        mainActivity.myAutoComplete.setAdapter(mainActivity.myAdapter);
+        searchActivity.myAdapter.notifyDataSetChanged();
+        searchActivity.myAdapter = new ArrayAdapter<String>(searchActivity, R.layout.autocomplete_list_row, searchActivity.item);
+        searchActivity.myAutoComplete.setAdapter(searchActivity.myAdapter);
     }
 }
--- app/src/main/java/com/snumap/snumapversion1/SearchRouteActivity.java
+++ app/src/main/java/com/snumap/snumapversion1/SearchRouteActivity.java
@@ -1,23 +1,83 @@
 package com.snumap.snumapversion1;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SearchRouteActivity extends AppCompatActivity {
     TextView txtDelete;
     Button btnChange;
     FloatingActionButton fab;
 
+    private InputMethodManager imm; // for keybaord control
+
+    // db 처리하는 부분
+    public MyDatabase db;
+
+    CustomAutoCompleteView myAutoComplete;
+
+    // adapter for auto-complete
+    ArrayAdapter<String> myAdapter;
+
+    // just to add some initial value
+    ArrayList<String> item = new ArrayList<String>();
+
+    // 초성 검색을 위한 arrayList
+    ArrayList<String> searchList = new ArrayList<String>();
+
+    ArrayList<DataSet> search_result = new ArrayList<DataSet>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_route);
+
+        // 키보드 관리를 위한 시작, 아래 함수 있다.
+        init();
+
+        // 디비 처리 하는 부분 시작
+        db = new MyDatabase(this);
+        addList();//검색리스트를 애드 해준다
+
+        // autocompletetextview is in activity_main.xml
+        myAutoComplete = (CustomAutoCompleteView) findViewById(R.id.myAutoCompleteSR);
+
+        // add the listener so it will tries to suggest while the user types
+        myAutoComplete.addTextChangedListener(new SRCustomAutoCompleteTextChangedListener(this));
+
+        // set our adapter
+        myAdapter = new ArrayAdapter<String>(this, R.layout.autocomplete_list_row, item);
+        myAutoComplete.setAdapter(myAdapter); //....여기까지(디비)
+
+        // 자동검색 바 클릭시 검색어 없애기
+        myAutoComplete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAutoComplete.setText("");
+                showKeyboard();
+            }
+        });
+
+        // 자동 검색 리스트에서 검색어 클릭 시 해당 위치로 이동
+        myAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                hideKeyboard();
+            }
+        });
 
         txtDelete = (TextView) findViewById(R.id.txtDelete);
         btnChange = (Button) findViewById(R.id.btnChange);
@@ -32,5 +92,23 @@ public class SearchRouteActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext(), "FAB Clicked", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void addList() {
+        searchList = db.insertBuildingName();
+    }
+
+    private void init(){
+        imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        myAutoComplete = (CustomAutoCompleteView) findViewById(R.id.myAutoCompleteSR);
+    }
+
+    private void showKeyboard(){
+        imm.showSoftInput(myAutoComplete, 0);
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
