20c2b9e26948b2ce5ffd0dd3171671b44df44d5d
RYU KI HYUN <rukeon@snu.ac.kr>
2015-08-27 17:39:19 +0900

    FAB 등록 및 setVisibility 되는 것 확인

--- app/src/main/java/com/snumap/snumapversion1/SearchRouteActivity.java
+++ app/src/main/java/com/snumap/snumapversion1/SearchRouteActivity.java
@@ -1,16 +1,18 @@
 package com.snumap.snumapversion1;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SearchRouteActivity extends AppCompatActivity {
     TextView txtDelete;
     Button btnChange;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,30 +21,17 @@ public class SearchRouteActivity extends AppCompatActivity {
 
         txtDelete = (TextView) findViewById(R.id.txtDelete);
         btnChange = (Button) findViewById(R.id.btnChange);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
         String text = "최근 검색기록 <font color='red'>삭제</font>";
         txtDelete.setText(Html.fromHtml(text), TextView.BufferType.SPANNABLE);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search_route, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Your FAB click action here...
+                Toast.makeText(getBaseContext(), "FAB Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
